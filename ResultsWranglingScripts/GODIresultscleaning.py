20b16b668e5ee8e87a9c5a087b702fe28385e069
CJuice <CJuice@users.noreply.github.com>
2019-05-21 12:04:41 -0400

    documenting and cleanup

--- ResultsWranglingScripts/GODIresultscleaning.py
+++ ResultsWranglingScripts/GODIresultscleaning.py
@@ -1,4 +1,13 @@
 """
+Clean GODI results to address changes in feature class names but no change to actual data values.
+
+Changes to feature class names and presence in the geodatabase have lead to some poor quality data in our sde. GODI
+inspects the geodatbase monthly and provides metrics on the contents. As we clean up our practices and standards the
+historical runs will be out of sync with the new runs for the same datasets. This script assists with cleaning the
+historical runs so that they are consistent with modern names. For instance, a feature class previously named
+my_feature_class may have been updated to my_feature_class_DOIT. This will result in two separate visualizations with
+non-overlapping date ranges when really this is the same dataset and the results should be visualized together, not as
+two separate datasets.
 
 NOTE: When dataset is pulled from Socrata, the numbers come with thousands separator commas. These will cause issues
 unless removed before processing. In Excel, change the column type to Numbers with no thousand separators or replace
@@ -13,10 +22,12 @@ def main():
     import pandas as pd
 
     # VARIABLES
-    godi_results_dataset_level_csv = r"GODI_datasets_results_cleaned.csv"
-    godi_results_field_level_csv = r"GODI_fields_results_cleaned.csv"
+    cols_to_change_datasetlevel = ["Name", "FC_ID", "ROW_ID"]
+    cols_to_change_fieldlevel = ["FLD_ID", "FC_ID", "ROW_ID"]
     godi_dataset_output_csv = r"GODI_datasets_cleaned_output.csv"
     godi_field_output_csv = r"GODI_fields_cleaned_output.csv"
+    godi_results_dataset_level_csv = r"GODI_datasets_results_cleaned.csv"
+    godi_results_field_level_csv = r"GODI_fields_results_cleaned.csv"
     replace_dict = {'SOCI_SportVenues_Archery': 'SOCI_Archery_MDSports',
                     'SOCI_SportVenues_AutoRacing': 'SOCI_AutoRacing_MDSports',
                     'SOCI_SportVenues_BMX': 'SOCI_BMX_MDSports',
@@ -98,7 +109,23 @@ def main():
                     'SOCI_SportVenues_Wrestling': 'SOCI_Wrestling_MDSports'}
 
     # FUNCTIONS
-    def revise_names(godi_value):
+    def replace_values(dataframe: pd.DataFrame, column_list: list) -> pd.DataFrame:
+        """
+        Apply custom function to column series
+        :param dataframe: dataframe from csv file of results from GODI runs
+        :param column_list: list of column names in the results dataset that need the function applied
+        :return: dataframe with cleaned values
+        """
+        for column in column_list:
+            dataframe[column] = dataframe[column].apply(func=revise_names)
+        return dataframe
+
+    def revise_names(godi_value: str) -> str:
+        """
+        Replace sought value with sustitute, focusing on sportvenues at time of design.
+        :param godi_value: str data value from columns series
+        :return: string
+        """
         if "sportvenues" in godi_value.lower():
             pass
         else:
@@ -114,24 +141,16 @@ def main():
 
         return godi_value
 
-    def replace_values(dataframe, column_list):
-        for column in column_list:
-            # print(f"\n{column}")
-            dataframe[column] = dataframe[column].apply(func=revise_names)
-        return dataframe
-
     # FUNCTIONALITY
     # GODI Dataset Level
     df_datasets_level = pd.read_csv(filepath_or_buffer=godi_results_dataset_level_csv)
-    print(df_datasets_level.info())
-    cols_to_change_datasetlevel = ["Name", "FC_ID", "ROW_ID"]
+    # print(df_datasets_level.info())
     df_datasets_level = replace_values(dataframe=df_datasets_level, column_list=cols_to_change_datasetlevel)
     df_datasets_level.to_csv(path_or_buf=godi_dataset_output_csv, index=False)
 
     # GODI Field Level
     df_field_level = pd.read_csv(filepath_or_buffer=godi_results_field_level_csv)
-    print(df_field_level.info())
-    cols_to_change_fieldlevel = ["FLD_ID", "FC_ID", "ROW_ID"]
+    # print(df_field_level.info())
     df_field_level = replace_values(dataframe=df_field_level, column_list=cols_to_change_fieldlevel)
     df_field_level.to_csv(path_or_buf=godi_field_output_csv, index=False)
 
